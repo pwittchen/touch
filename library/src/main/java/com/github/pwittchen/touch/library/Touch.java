ebc44dad1d05104ca9c70b08975d9f91d0ae5322
Piotr Wittchen <piotr@wittchen.io>
2020-04-05 23:02:50 +0200

    refactoring the code and adding javadocs

--- library/src/main/java/com/github/pwittchen/touch/library/Touch.java
+++ library/src/main/java/com/github/pwittchen/touch/library/Touch.java
@@ -8,62 +8,54 @@ import io.reactivex.FlowableEmitter;
 public class Touch {
   private FlowableEmitter<TouchEvent> emitter;
 
+  /**
+   * observes touch events with RxJava Flowable using BUFFER backpressure strategy
+   * Remember to call {@link #dispatchTouchEvent(MotionEvent) dispatchTouchEvent} method as well.
+   *
+   * @return Flowable with the stream of touch events
+   */
   public Flowable<TouchEvent> observe() {
     return observe(BackpressureStrategy.BUFFER);
   }
 
+  /**
+   * observes touch events with RxJava Flowable.
+   * Remember to call {@link #dispatchTouchEvent(MotionEvent) dispatchTouchEvent} method as well.
+   *
+   * @param backpressureStrategy backpressure strategy for RxJava Flowable
+   * @return Flowable with the stream of touch events
+   */
   public Flowable<TouchEvent> observe(BackpressureStrategy backpressureStrategy) {
     return Flowable.create(emitter -> Touch.this.emitter = emitter, backpressureStrategy);
   }
 
   /**
    * Called to process touch screen events.
+   * Required to invoke stream of Flowables.
    *
    * @param event MotionEvent
    */
   public boolean dispatchTouchEvent(final MotionEvent event) {
-    checkNotNull(event, "event == null");
-    boolean isEventConsumed = false;
-
     switch (event.getAction()) {
       case MotionEvent.ACTION_DOWN: // user started touching the screen
-        onActionDown(event);
+        onNextSafely(new TouchEvent(event.getX(), event.getY(), TouchType.DOWN));
         break;
       case MotionEvent.ACTION_UP:   // user stopped touching the screen
-        isEventConsumed = onActionUp(event);
+        onNextSafely(new TouchEvent(event.getX(), event.getY(), TouchType.UP));
         break;
       case MotionEvent.ACTION_MOVE: // user is moving finger on the screen
-        onActionMove(event);
+        onNextSafely(new TouchEvent(event.getX(), event.getY(), TouchType.MOVE));
         break;
       default:
         break;
     }
 
-    return isEventConsumed;
-  }
-
-  private void onActionDown(final MotionEvent event) {
-    onNextSafely(new TouchEvent(event.getX(), event.getY(), TouchType.DOWN));
-  }
-
-  private boolean onActionUp(final MotionEvent event) {
-    onNextSafely(new TouchEvent(event.getX(), event.getY(), TouchType.UP));
     return true;
-  }
-
-  private void onActionMove(final MotionEvent event) {
-    onNextSafely(new TouchEvent(event.getX(), event.getY(), TouchType.MOVE));
   }
 
   private void onNextSafely(final TouchEvent touchEvent) {
     if (emitter != null) {
       emitter.onNext(touchEvent);
-    }
-  }
-
-  private void checkNotNull(final Object object, final String message) {
-    if (object == null) {
-      throw new IllegalArgumentException(message);
     }
   }
 }
